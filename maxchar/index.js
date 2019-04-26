5f8652ec2274dfc1699a5c722c5e1d7fb954ddd0
terence1991 <cplterencehawes@gmail.com>
2019-04-26 16:21:28 -0400

    character object built

--- maxchar/index.js
+++ maxchar/index.js
@@ -6,15 +6,34 @@
 // maxChar("apple 1231111") === "1"
 
 function maxChar(str) {
-    const chars = {}
-    for(let char in str) {
-        if(chars[char]) {
-            chars[char]++
+    
+    // helper variable 
+    const charMap = {}
+    //increment gradually 
+    let max = 0
+    let maxChar = ''
+    
+    // loop through string. if index of string exists in key of object increment it by one.
+    // else/ it does not  asign it to just one
+    for(let char of str) {
+        if(charMap[char]) {
+            charMap[char]++;
         } else {
-            chars[char] = 1
+            charMap[char] = 1;
         }
     }
 
+    //once char map is built. Iterate through the object propertys.
+    // if property excedes value of helper var of max. 
+    // Asign max so it increments Gradually in conjucntion with the given string interation
+    for (let char in charMap) {
+        if(charMap[char] > max) {
+            max = charMap[char]; 
+            maxChar = char;
+        }
+    }
+    return maxChar
 }
+
 
 module.exports = maxChar;
