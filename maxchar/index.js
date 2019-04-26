a7647d92c4772734653ea431a827973a0dda4dc2
terence1991 <cplterencehawes@gmail.com>
2019-04-26 14:59:39 -0400

    char question

--- maxchar/index.js
+++ maxchar/index.js
@@ -5,6 +5,16 @@
 // maxChar("abcccccccd") === "c"
 // maxChar("apple 1231111") === "1"
 
-function maxChar(str) {}
+function maxChar(str) {
+    const chars = {}
+    for(let char in str) {
+        if(chars[char]) {
+            chars[char]++
+        } else {
+            chars[char] = 1
+        }
+    }
+
+}
 
 module.exports = maxChar;
