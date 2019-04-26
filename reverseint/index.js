354b61106e08bd935934339b35ffde0ba1671a14
terence1991 <cplterencehawes@gmail.com>
2019-04-26 14:29:49 -0400

    parsed int worked out

--- reverseint/index.js
+++ reverseint/index.js
@@ -8,6 +8,27 @@
 //   reverseInt(-15) === -51
 //   reverseInt(-90) === -9
 
-function reverseInt(n) {}
+function reverseInt(n) {
+    const reversed = n.toString().split('').reverse().join('')
+
+    // Math.sign(n) to check if n is negative or positive 
+    // timeesing it by itself will return proper value
+    return parseInt(reversed) * Math.sign(n)
+}
+
 
 module.exports = reverseInt;
+
+
+
+
+// simple solution 
+// function reverseInt(n) {
+//     const reversed = n.toString().split('').reverse().join('')
+//     if(n < 0) {
+//         return parseInt(reversed) * -1;
+//     } else {
+//         return  parseInt(reversed)
+//     }
+//     }
+// console.log(reverseInt(55))
