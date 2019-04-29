8bcff131c6ee3a9f92d42bc5fb541b349bbb201f
terence1991 <cplterencehawes@gmail.com>
2019-04-29 11:29:33 -0400

    implemented anaigram solution

--- anagrams/index.js
+++ anagrams/index.js
@@ -8,6 +8,31 @@
 //   anagrams('RAIL! SAFETY!', 'fairy tales') --> True
 //   anagrams('Hi there', 'Bye there') --> False
 
-function anagrams(stringA, stringB) {}
+function anagrams(stringA, stringB) {
+    const charMap1 = buildCharMap(stringA)
+    const charMap2 = buildCharMap(stringB)
+
+    if(Object.keys(charMap1).length !== Object.keys(charMap2).length) {
+        return false;
+    } else {
+        for (let char in charMap1) {
+            if (charMap1[char] !== charMap2[char]) {
+                return false 
+            } else {
+                return true 
+            }
+        }
+    }
+}
+
+function buildCharMap(str) {
+    const charMap = {};
+
+    for(let char of str.replace(/[^\w]/).toLowerCase()) {
+        charMap[char] = charMap[char] + 1 || 1;
+    }
+    return charMap;
+}
+
 
 module.exports = anagrams;
