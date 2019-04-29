51ffa1a51763360e5031c47c20356c9c3b12892e
terence1991 <cplterencehawes@gmail.com>
2019-04-29 10:46:01 -0400

    chunking area using slice

--- chunk/index.js
+++ chunk/index.js
@@ -9,21 +9,38 @@
 // chunk([1, 2, 3, 4, 5], 10) --> [[ 1, 2, 3, 4, 5]]
 
 function chunk(array, size) {
-    // initiliaze empty arrary
-   const chunked = []
-    // loop over paramter
-    array.forEach(element => {
-        // find last emlement of empty arrary 
-        const lastElement = chunked[chunked.length - 1]
-        // if last element arrar does not exist or its size equals parameter
-        if(!lastElement || lastElement.length === size) {
-            // push the current index onto the array( remember to put the element in arrary brackets)
-            chunked.push([element])
-        } else {
-            lastElement.push(element)
-        }
-    });
-    return chunked; 
+    const chunked = [];
+    // make sure to make it let cause index will be changing over time 
+    let index = 0
+
+    while(index < array.length) {
+        // starting value to what the index changes to over time over growing array  
+        chunked.push(array.slice(index, index + size))
+        // drives code so increment by sie 
+        index += size
+    }
+    return chunked;
 }
 
 module.exports = chunk;
+
+
+
+// first solution 
+// function chunk(array, size) {
+//     // initiliaze empty arrary
+//    const chunked = []
+//     // loop over paramter
+//     array.forEach(element => {
+//         // find last emlement of empty arrary 
+//         const lastElement = chunked[chunked.length - 1]
+//         // if last element arrar does not exist or its size equals parameter
+//         if(!lastElement || lastElement.length === size) {
+//             // push the current index onto the array( remember to put the element in arrary brackets)
+//             chunked.push([element])
+//         } else {
+//             lastElement.push(element)
+//         }
+//     });
+//     return chunked; 
+// }
