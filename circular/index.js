be50b7a8928e24a5ecea55d1fb693ca0ed98a232
terence1991 <cplterencehawes@gmail.com>
2019-05-03 12:40:39 -0400

    looped solution implemented

--- circular/index.js
+++ circular/index.js
@@ -12,6 +12,20 @@
 //   c.next = b;
 //   circular(l) // true
 
-function circular(list) {}
+function circular(list) {
+    let fast = this.head;
+    let slow = this.head;
+
+    while(fast.next && fast.next.next) {
+         slow = slow.next
+         fast = fast.next.next;
+
+         if(slow === fast ) {
+             return true;
+         }
+    }
+
+    return false 
+}
 
 module.exports = circular;
