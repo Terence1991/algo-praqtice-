fae714983c2fd644b38546ab317fec3a5f3854c0
terence1991 <cplterencehawes@gmail.com>
2019-05-07 14:41:50 -0400

    merge sort with recursion

--- sorting/index.js
+++ sorting/index.js
@@ -34,6 +34,15 @@ function selectionSort(arr) {
 }
 
 function mergeSort(arr) {
+    if(arr.length === 1) {
+        return arr; 
+    }
+
+    const center = Math.floor(arr.length / 2)
+    const left = arr.slice(0, center)
+    const right = arr.slice(center)
+
+    return merge(merSort(left), mergeSort(right)) 
   
 }
 
